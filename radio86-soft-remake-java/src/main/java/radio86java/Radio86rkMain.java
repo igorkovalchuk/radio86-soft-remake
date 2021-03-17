1ad0648d9914bbf2957855f041d4e619b5f3435f
Igor Kovalchuk <igorko76@gmail.com>
2021-03-17 21:51:20 +0200

    minor refactoring

--- radio86-soft-remake-java/src/main/java/radio86java/Radio86rkMain.java
+++ radio86-soft-remake-java/src/main/java/radio86java/Radio86rkMain.java
@@ -1,31 +1,29 @@
 package radio86java;
 
 //import radio86java.basic.Basic;
-
 import radio86java.uiswing.UserInterfaceImpl;
-import java.io.InputStream;
-import java.util.Scanner;
-
 
 public class Radio86rkMain {
 
-    public static void main(String[] argv) {
+  public static void main(String[] argv) {
 
-        final UserInterfaceImpl screen = new UserInterfaceImpl();
+    final UserInterfaceImpl screen = new UserInterfaceImpl();
 
-        //String listing = Utils.loadResource("ohota_na_lis.js");
-        //screen.getEditor().setText(listing.toString());
+    java.awt.EventQueue.invokeLater(() -> {
+      screen.setVisible(true);
+      screen.getCanvas().requestFocus();
+    });
 
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            @Override
-            public void run() {
-                screen.setVisible(true);
-                screen.getCanvas().requestFocus();
-            }
-        });
+  }
 
-        //Basic basic = new Basic();
-        //basic.run(listing, screen);
-    }
+  //private static void setDefaultListing(UserInterfaceImpl screen) {
+    //String listing = Utils.loadResource("ohota_na_lis.js");
+    //screen.getEditor().setText(listing.toString());
+  //}
+
+  //private static void runBasicCode(String listing, UserInterfaceImpl screen) {
+    //Basic basic = new Basic();
+    //basic.run(listing, screen);
+  //}
 
 }
